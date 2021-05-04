7eea071c5c4e2ab90869f178f2675c93936226ee
Ivan Kovalenko <s1ckretbox@gmail.com>
2021-05-04 13:40:06 +0300

    feat(client-service): add user service
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

---
+++ client/src/main/java/com/s1cket/labs/client/service/UserService.java
@@ -0,0 +1,32 @@
+package com.s1cket.labs.client.service;
+
+import com.s1cket.labs.client.model.dao.UserEntity;
+import com.s1cket.labs.client.model.dto.UserDto;
+import com.s1cket.labs.client.model.mapper.UserMapper;
+import com.s1cket.labs.client.model.repository.UserRepository;
+import com.s1cket.labs.client.service.exception.ServiceException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    private final UserMapper userMapper;
+
+    public UserDto findByLogin(String login) throws ServiceException {
+        UserEntity userEntity;
+
+        try {
+            userEntity = userRepository.findByLogin(login).orElseThrow(EntityNotFoundException::new);
+        } catch (Exception ex) {
+            throw new ServiceException("User " + login + " is not found.", ex);
+        }
+
+        return userMapper.userEntityToDto(userEntity);
+    }
+}
