29bd57996d06e985b02fe31c20fd829241fd6338
Ivan Kovalenko <s1ckretbox@gmail.com>
2021-05-21 15:18:59 +0300

    feat(client): add web service
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

---
+++ client/src/main/java/com/s1cket/labs/client/service/RegistrationService.java
@@ -0,0 +1,27 @@
+package com.s1cket.labs.client.service;
+
+import com.s1cket.labs.client.model.dto.UserRegistrationDto;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class RegistrationService {
+    private final WebClient webClient;
+
+    public RegistrationService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("http://localhost:8080").build();
+    }
+
+    public UserRegistrationDto registerUser(UserRegistrationDto userRegistrationDto) {
+        return this.webClient.post()
+                .uri("/user/register")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(Mono.just(userRegistrationDto), UserRegistrationDto.class)
+                .retrieve()
+                .bodyToMono(UserRegistrationDto.class)
+                .block();
+    }
+}
