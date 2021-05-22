a4c43974734b4063a0fee83e60d050d9f2ad6ed6
Ivan Kovalenko <s1ckretbox@gmail.com>
2021-05-22 22:59:39 +0300

    feat(server): improve inital data script
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- node/src/main/resources/data.sql
+++ node/src/main/resources/data.sql
@@ -1,4 +1,7 @@
 insert into user_entity (id, login, password, address)
     values
         (1, 's1ckret', '1', 'ff0a1ae640a87d48baf91011db16d2e70ae628c2'),
-        (2, 'tanos', '2', '7960d74d75b70095301a102ec83e63312589e885');
+        (2, 'tanos', '2', '7960d74d75b70095301a102ec83e63312589e885'),
+        (3, 'Anrew', '3', '6257e4c53f4435cec476dfcb4b090d7784eb45e3'),
+        (4, 'Levy', '4', 'fe736889711940fb99ba5df8b92ed85b1eeb49d5');
+
